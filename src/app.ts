5183cfbdaa14739925b3bee19a0fd868d9c79445
douglas.fonseca <douglas.hfonseca@gmail.com>
2021-05-17 00:51:40 -0300

    corrigindo cors

--- src/app.ts
+++ src/app.ts
@@ -18,11 +18,11 @@ createConnection();
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.use(cors());
 app.use(express.json());
 
 app.use(
